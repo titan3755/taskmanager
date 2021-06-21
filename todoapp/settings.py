bc97e8cee222f56fe8da36f9887da838689fc30b
Titan <68808479+titan3755@users.noreply.github.com>
2021-06-21 12:38:13 +0600

    rteterer

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 import dj_database_url
